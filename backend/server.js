dbf4a0ce1d9dc43a92a0d98c7e8291b9907e0018
rushimanche <61168471+rushimanche@users.noreply.github.com>
2021-07-14 13:43:11 -0400

    removed fsevents

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
 // allow cors requests from any origin and with credentials
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
@@ -18,6 +21,11 @@ app.use('/accounts', require('./accounts/accounts.controller'));
 
 // file api routes
 app.use('/files', require('./files/files.controller'));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // global error handler
 app.use(errorHandler);
